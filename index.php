d294debbf5106cc363979c443fe1bf5937f91d37
jackyfox <fox.sawyer@gmail.com>
2011-05-06 15:21:43 +0400

    Work everything except card color chanhing

--- index.php
+++ index.php
@@ -11,35 +11,90 @@
 <title>Black or white</title>
 </head>
 <body>
-<div class="container showgrid">
+<div class="container">
+	<center>
   	<div class="span-24">
-  	<center>
   		<h1>Черная или белая?</h1>
-  	</center>
   	</div>
-
+	
   	<div class="span-8 prepend-8 append-8">
-  		<div class="card">
-  			<h2>Место для карты</h2>
+  		<div class="card" style="padding: 10px">
+  			<img alt="Card" src="images/card_back.svg" width="300">
   		</div>
   		<div class="buttons">
-  			<h2>Место для кнопок</h2>
+  			<button name="black">Black</button>
+  			<button name="white">White</button>
   		</div>
-  		<div class="stat">
-  			<h2>Место для статистики</h2>
+  		<div class="stat" style="padding: 10px">
+  			<p>Правильно: <span id="right">0</span> <br />
+  			Всего попыток: <span id="total">0</span></p>
   		</div>
   	</div>
   	
   	<div class="span-24">
-  	<center>
   		<p>All rights ok &copy; 2011 <a href="http://www.jackyfox.com">jackyfox</a><br />
   		Powered on <a href="http://jquery.com/">jQuery</a> and <a href="http://www.blueprintcss.org/">Blueprint</a></p>
-  	</center>
   	</div>
+  	</center>
   </div>
 <script type="text/javascript">
+var card;
+var attempts = 0;
+var guesses = 0;
+function new_card()
+{
+	return Math.round(Math.random());
+}
+function attempt(btn)
+{
+	attempts++;
+	card = new_card();
+
+	if (card == btn)
+	{
+		guesses++;
+		$("span#right").text(guesses);
+	}
+	$("span#total").text(attempts);
+	
+	// card rotation step 1
+	$(".card img").animate({
+		width: 1,
+		height: 450
+	}, 150 );
+
+	// change card color to black or white
+	if (card == 0)
+		$(".card img").attr("src","images/card_black.svg");
+	else
+		$(".card img").attr("src","images/card_white.svg");
+	
+	// card rotation step 2
+	$(".card img").animate({
+		width: 300,
+		height: 450
+	}, 150 ).delay(500).animate({
+		width: 1,
+		height: 450
+	}, 150 );
+	
+	// return card back
+	$(".card img").attr("src","images/card_back.svg");
+
+	// card rotation step 3
+	$(".card img").animate({
+		width: 300,
+		height: 450
+	}, 150 );
+
+}
 $(document).ready(function() {
-	//alert('Hello, user!');
+	$("button[name='black']").click(function() {
+		attempt(0);
+	});
+	$("button[name='white']").click(function() {
+		attempt(1);
+	});
 });
  </script>
 </body>
