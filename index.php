4897d03d3fc1c1f75be4d24e3315a6868300f509
jackyfox <fox.sawyer@gmail.com>
2011-05-06 11:53:26 +0400

    Scaffold divs, blueprint and jQuery added

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" href="css/screen.css" type="text/css" media="screen, projection">
+<link rel="stylesheet" href="css/print.css" type="text/css" media="print"> 
+<!--[if lt IE 8]>
+  <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen, projection">
+<![endif]-->
+<script type="text/javascript" src="js/jquery-1.6.min.js"></script>
+<title>Black or white</title>
+</head>
+<body>
+<div class="container showgrid">
+  	<div class="span-24">
+  	<center>
+  		<h1>Черная или белая?</h1>
+  	</center>
+  	</div>
+
+  	<div class="span-8 prepend-8 append-8">
+  		<div class="card">
+  			<h2>Место для карты</h2>
+  		</div>
+  		<div class="buttons">
+  			<h2>Место для кнопок</h2>
+  		</div>
+  		<div class="stat">
+  			<h2>Место для статистики</h2>
+  		</div>
+  	</div>
+  	
+  	<div class="span-24">
+  	<center>
+  		<p>All rights ok &copy; 2011 <a href="http://www.jackyfox.com">jackyfox</a><br />
+  		Powered on <a href="http://jquery.com/">jQuery</a> and <a href="http://www.blueprintcss.org/">Blueprint</a></p>
+  	</center>
+  	</div>
+  </div>
+<script type="text/javascript">
+$(document).ready(function() {
+	//alert('Hello, user!');
+});
+ </script>
+</body>
+</html>
