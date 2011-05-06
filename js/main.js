b32d4378e738d14b0b144a6a7ee87fcb8503aa77
jackyfox <fox.sawyer@gmail.com>
2011-05-06 17:03:35 +0400

    Save current result into cookies

--- js/main.js
+++ js/main.js
@@ -8,6 +8,16 @@ function new_card()
 {
 	return Math.round(Math.random());
 }
+function clear()
+{
+	attempts = 0;
+	guesses  = 0;
+	$("span#right").text(guesses);
+	$("span#total").text(attempts);
+	// remove cookies
+	$.cookie("bow_right", null);
+	$.cookie("bow_total", null);
+}
 function attempt(btn)
 {
 	attempts++;
@@ -19,6 +29,10 @@ function attempt(btn)
 		$("span#right").text(guesses);
 	}
 	$("span#total").text(attempts);
+	
+	// sets cookie
+	$.cookie("bow_right", guesses);
+	$.cookie("bow_total", attempts);
 	
 	// card rotation step 1
 	$(".card img").animate({
@@ -52,10 +66,23 @@ function attempt(btn)
 
 }
 $(document).ready(function() {
+	// if last results stored in cookies
+	if ($.cookie("bow_right"))
+	{
+		// restore them
+		guesses = $.cookie("bow_right");
+		attempts = $.cookie("bow_total");
+		
+		$("span#right").text(guesses);
+		$("span#total").text(attempts);
+	}
 	$("button[name='black']").click(function() {
 		attempt(0);
 	});
 	$("button[name='white']").click(function() {
 		attempt(1);
+	});
+	$("button[name='clear']").click(function() {
+		clear();
 	});
 });
